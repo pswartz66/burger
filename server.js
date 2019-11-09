a5293bde6f80a3bc44013ef0e8eeb300fa444f7d
Phil Swartz <pswartz.sju@gmail.com>
2019-11-08 22:32:53 -0500

    added code to server.js file

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them
+const routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
+
+// Start the sever so that it can begin listening to client side user input
+app.listen(PORT, function() {
+
+    console.log("Server listening on: http://localhost:" + PORT);
+    
+
+});
